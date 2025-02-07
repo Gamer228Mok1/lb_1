8c75b5c0a2b11e8f5e11e86132d9072901805a89
DmitriyKwelov <kwelov.work@gmail.com>
2025-02-07 20:36:40 +0300

    Добавьте файлы проекта.

---
+++ lb_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using lb_1.Models;
+using Microsoft.EntityFrameworkCore;
+namespace FoodStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+        DbContextOptions< ApplicationDbContext> options) : base(options)
+{
+}
+    public DbSet<Category> Category { get; set; }
+}
+}
